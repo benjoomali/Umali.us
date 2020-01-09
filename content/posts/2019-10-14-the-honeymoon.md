4b3c624775d7f69986a058366d9556294ab221ec
Ben Umali <benjoomali@gmail.com>
2020-01-09 10:30:05 -0800

    the first commit

---
+++ content/posts/2019-10-14-the-honeymoon.md
@@ -0,0 +1,33 @@
+---
+title: "The Honeymoon"
+slug: the-honeymoon
+description: "You can view some pictures from our honeymoon to Italy."
+date: 2019-10-14 17:54:43
+author: Ben and Danielle
+tags: 
+    - honeymoon
+cover: /images/posts/honeymoon_bg.jpg
+fullscreen: true
+---
+**This page is still under construction. More pictures and details to come!**
+
+We had a fantastic time on our two week honeymoon to Italy. Please enjoy some of the pictures from the trip below. Thank you all for your support and and your love!
+
+## Rome, Italy
+After the wedding, we took off to the airport to fly to Rome, Italy. 
+## Newly Wed Blessing at The Vatican 
+Danielle did a great job getting us tickets and planning a time to get us to the Vatican. Once a month, the Pope will bless newly weds during his weekday "service" where he addresses the people. Many from all over fly in with their Wedding dresses and Suits in order to get blessed.
+
+![The Vatican](/images/posts/vatican.jpg)
+
+![Waiting for Pope Francis](/images/posts/waiting_for_the_pope.jpg)
+
+Unfortunately, the best we could do was get this close to Papa Francis. It was still an experience and we were grateful to get his blessing.
+
+![Picture of many trying to greet Pope Francis](/images/posts/papa_francis.jpg)
+## Siena, Tuscany
+## Florence
+## Positano
+![On a Boat Heading Towards Positano](/images/posts/honeymoon_cover.jpg)
+
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,128 @@
+<template>
+  <Layout class="bg-white">
+    <main>
+      <header class="bg-blue-500 bg-cover bg-center opacity-75" style="background-image:url('/images/hero-image-umali-us-1.jpg')">
+        
+        <div class="max-w-xl md:max-w-3xl xl:max-w-4xl mx-auto text-center px-6 py-10 md:py-32">
+          <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-extrabold mt-5">
+            <g-link to="/" class="text-red-700">The Umalis</g-link>
+          </h1>
+
+
+        </div>
+      </header>
+      <div class="max-w-xl md:max-w-3xl xl:max-w-4xl mx-auto text-center px-6 py-4 md:py-12 border-b border-gray-300">
+        <h2 class="text-gray-800 text-lg sm:text-3xl font-sans font-bold text-center">Pictures from the Wedding, Honeymoon and More!</h2>
+        <h2 class="text-gray-700 text-md sm:text-2xl font-sans text-center">Thank you always for your support and love.</h2>
+        </div>
+        <section>
+            <article>
+            <div class="mx-auto max-w-3xl bg-gray-100 px-6">
+              <div class="py-8 sm:py-20 border-b border-gray-300">
+                <header class="text-center mb-8">
+                  <div class="text-gray-700 text-xs mb-2 uppercase">12 October, 2019</div>
+                  <h2 class="text-3xl sm:text-4xl leading-tight font-sans mb-1 sm:mb-2">
+                    <span class="text-black font-bold"><a target="_blank" href="https://three16photography.client-gallery.com/gallery/umali-wedding-or-october-12th-2019">The Wedding</a></span>
+                  </h2>
+                </header>
+                <p class="leading-normal text-gray-700 text-lg px-2 sm:px-4 md:px-10"><strong>Three16 Photography</strong> did a fantastic job shooting our wedding! If you'd like to see some of the pictures, you can visit their website. Video to come soon.</p>
+                <p class="leading-normal text-gray-700 text-lg px-2 py-2 sm:px-4 md:px-10"><a class="underline font-bold" target="_blank" href="https://three16photography.client-gallery.com/gallery/umali-wedding-or-october-12th-2019">You can view the photos here.</a></p>
+              </div>
+            </div>
+          </article>
+        </section>
+      <section>
+        <post-item v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node" />
+      </section>
+      <pagination :info="$page.posts.pageInfo" v-if="$page.posts.pageInfo.totalPages > 1" />
+      <site-footer class="py-8 sm:py-16" />
+    </main>
+  </Layout>
+</template>
+
+<script>
+import config from '~/.temp/config.js'
+import SiteFooter from '@/components/Footer'
+import PostItem from '@/components/PostItem'
+import Pagination from '@/components/Pagination'
+
+export default {
+  components: {
+    PostItem,
+    Pagination,
+    SiteFooter,
+  },
+  metaInfo () {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        { property: "og:type", content: 'website' },
+        { property: "og:title", content: this.$static.metadata.siteName },
+        { property: "og:description", content: this.$static.metadata.siteDescription },
+        { property: "og:url", content: this.$static.metadata.siteUrl },
+        { property: "og:image", content: this.ogImageUrl },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: this.$static.metadata.siteName },
+        { name: "twitter:description", content: this.$static.metadata.siteDescription },
+        { name: "twitter:site", content: "@cossssmin" },
+        { name: "twitter:creator", content: "@cossssmin" },
+        { name: "twitter:image", content: this.ogImageUrl },
+      ],
+    }
+  },
+  computed: {
+    config () {
+      return config
+    },
+    ogImageUrl () {
+      return `${this.config.siteUrl}/images/bleda-card.png`
+    }
+  },
+}
+</script>
+
+<page-query>
+  query Home ($page: Int) {
+    posts: allPost (page: $page, perPage: 6) @paginate {
+      totalCount
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          id
+          title
+          timeToRead
+          datetime: date (format: "YYYY-MM-DD HH:mm:ss")
+          content
+          excerpt
+          description
+          path
+          cover
+          tags {
+            id
+            title
+            path
+          }
+          author {
+            id
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+</page-query>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteUrl
+    siteDescription
+  }
+}
+</static-query>
